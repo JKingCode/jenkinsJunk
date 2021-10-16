5eb04bf6bad0333a7fe0c2debf9204851feb168b
Josh King <josh.king793@gmail.com>
2021-10-15 18:07:31 -0600

    add fib and int to binary

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -39,7 +39,24 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        return 0;
+        /* Declare an array to store Fibonacci numbers. */
+        int f[] = new int[n + 1];
+        int i;
+
+        /* 0th and 1st number of the series are 0 and 1*/
+        f[0] = 0;
+
+        if (n > 0) {
+            f[1] = 1;
+
+            for (i = 2; i <= n; i++) {
+                /* Add the previous 2 numbers in the series
+             and store it */
+                f[i] = f[i - 1] + f[i - 2];
+            }
+        }
+
+        return f[n];
     }
 
 
@@ -51,7 +68,7 @@ class Calculator {
     if int a = 16 then this method returns: 10000
      */
     String intToBinaryNumber(int n){
-        return null;
+        return Integer.toBinaryString(n);
     }
 
     /*
